580088fa5f25d1d4f261dce57e8aff7c6689b976
Karthick Pannerselvam <karthickps.ai@gmail.com>
2019-10-02 23:19:35 +0530

    changes in rust maximum pairwise product

--- algorithmic-toolbox/week_1/2_maximum_pairwise_product/rust/src/main.rs
+++ algorithmic-toolbox/week_1/2_maximum_pairwise_product/rust/src/main.rs
@@ -1,3 +1,18 @@
-fn main() {
-    println!("Hello, world!");
+use std::io;
+
+fn main() -> io::Result<()> {
+    let mut size = String::new();
+    io::stdin()
+        .read_line(&mut size)
+        .expect("failed to read from stdin");
+    let mut size_trimmed = size.trim();
+    match size_trimmed.parse::<u32>() {
+        Ok(i) => {
+            for iter in 0..i {
+                println!("{}", iter);
+            }
+        }
+        Err(..) => println!("this was not an integer: {}", size_trimmed),
+    }
+    Ok(())
 }
