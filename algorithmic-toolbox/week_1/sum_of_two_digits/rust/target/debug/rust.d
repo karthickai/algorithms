c2176a56e20d2a661ad6fe10cd4724398017c9df
Karthick Pannerselvam <karthickps.ai@gmail.com>
2019-10-02 22:04:12 +0530

    Remove target directory

--- algorithmic-toolbox/week_1/sum_of_two_digits/rust/target/debug/rust.d
+++
@@ -1,1 +0,0 @@
-F:\PROJECTS\GIT\algorithms\algorithmic-toolbox\week_1\sum_of_two_digits\rust\target\debug\rust.exe: F:\PROJECTS\GIT\algorithms\algorithmic-toolbox\week_1\sum_of_two_digits\rust\src\main.rs
