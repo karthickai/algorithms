61e2c6db8dcea9a77b0865dbaf52f90226e36bae
Karthick Pannerselvam <karthickps.ai@gmail.com>
2019-10-04 00:33:47 +0530

    Maximum pairwise product both cpp and rust done

--- algorithmic-toolbox/week_1/2_maximum_pairwise_product/rust/src/main.rs
+++ algorithmic-toolbox/week_1/2_maximum_pairwise_product/rust/src/main.rs
@@ -1,37 +1,46 @@
+/// Maximum Pairwise Product
+/// Input
+/// 3
+/// 1 2 3
+/// Output
+/// 6
 use std::io;
-
+use std::convert::TryInto;
 
 fn string_to_int32(value: String) -> Result<i32, ()> {
     let value_trimmed = value.trim();
-    match value_trimmed.parse::<i32>(){
-    Ok(i) => Ok(i),
-    Err(..) => Err(()),
+    match value_trimmed.parse::<i32>() {
+        Ok(i) => Ok(i),
+        Err(..) => Err(()),
     }
 }
 
-fn main() -> io::Result<()> {
-    let mut size = String::new();
+fn read_line() -> Result<String, ()> {
+    let mut read = String::new();
     io::stdin()
-        .read_line(&mut size)
+        .read_line(&mut read)
         .expect("failed to read from stdin");
-    let size_trimmed = size.trim();
-    match size_trimmed.parse::<u32>() {
-        Ok(i) => {
-            for _iter in 0..i {
-                let mut values = String::new();
-                io::stdin()
-                    .read_line(&mut values)
-                    .expect("failed to read from stdin");
-                let mut vec_values: Vec<_> = values.split_ascii_whitespace().collect();
-                let mut final_vec: Vec<i32> = Vec::with_capacity(50);
-                for vec in vec_values {
-                    let val = string_to_int32(String::from(vec)).expect("unable to convert string to int");
-                    final_vec.push(val);
-                }
-                println!("{:?}", final_vec);
-            }
-        }
-        Err(..) => println!("this was not an integer: {}", size_trimmed),
+    Ok(read)
+}
+
+fn main() -> io::Result<()> {
+    let size_string = read_line().expect("unable to read string");
+    let size_int = string_to_int32(String::from(size_string)).expect("unable to parse string to int");
+    let size = size_int.try_into().unwrap();
+    let values = read_line().expect("unable to read string");
+    let vec_values: Vec<_> = values.split_ascii_whitespace().collect();
+    let mut final_vec: Vec<i32> = Vec::with_capacity(50);
+
+    for i in 0..size {
+        let val = string_to_int32(String::from(vec_values[i])).expect("unable to convert string to int");
+        final_vec.push(val);
     }
+
+    final_vec.sort();
+    let first = final_vec.last().unwrap().clone();
+    final_vec.pop();
+    let second = final_vec.last().unwrap().clone();
+    println!("{:?}", first * second);
+
     Ok(())
 }
